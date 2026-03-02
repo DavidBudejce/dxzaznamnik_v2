33082bbd8edaa96fd2ff1f6a0295dee20f01fa49
David Svoboda <davidak9900@gmail.com>
2026-03-02 16:56:01 +0100

    Add files via upload

--- service-worker_zal.js
+++ service-worker_zal.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "DxZaznamnik-v2";
+const CACHE_NAME = "DX_zaznamnik-v2.0";
 
 const FILES_TO_CACHE = [
   "./index.html",
@@ -16,19 +16,25 @@ const FILES_TO_CACHE = [
   "./icon-512.png"
 ];
 
+// Instalace SW + cache souborů
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// Aktivace SW + smazání starých cache
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
         keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
       )
     )
@@ -36,9 +42,12 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
+// Fetch – obsluha požadavků
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
